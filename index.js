3b89d875cd11f390ae4d618fc091b627faea263d
BikunGCA <38731672+anfony@users.noreply.github.com>
2024-08-14 13:47:18 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.set('port', 80)
+app.set('port', process.env.PORT || 3000); // Usar el puerto 3000 por defecto o un puerto especificado en el entorno
 
-app.use(express.static(path.join(__dirname,'src')));
+app.use(express.static(path.join(__dirname, 'src')));
 
 app.get('/', function(req, res){
-	res.send("Holaprros");
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(app.get('port'), ()=>{
-	console.log(`Aplicacion corriendo en el puerto ${app.get('port')}`);
+app.listen(app.get('port'), () => {
+    console.log(`Aplicacion corriendo en el puerto ${app.get('port')}`);
 });
